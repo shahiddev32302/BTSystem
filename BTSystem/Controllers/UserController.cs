337652871a512f4f5190b83a75327432bf4feaad
shahid3230 <shahidbilaldev@gmail.com>
2026-04-07 17:06:08 +0500

    Updated Changes in Route mangement  And Driver Management

---
+++ BTSystem/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Management()
+        {
+            return View();
+        }
+    }
+}
